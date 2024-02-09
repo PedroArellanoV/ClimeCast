a550bdd3c7c166166038510e9e45dd97e15ce6b5
pedro <pedro.arellano2001@gmail.com>
2024-02-09 21:52:22 +0000

    initial commit

---
+++ app/src/main/java/com/example/climecast/ClimeCastApp.kt
@@ -0,0 +1,7 @@
+package com.example.climecast
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ClimeCastApp: Application()
---
+++ app/src/main/java/com/example/climecast/ui/home/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.climecast.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.climecast.R
+import com.example.climecast.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
