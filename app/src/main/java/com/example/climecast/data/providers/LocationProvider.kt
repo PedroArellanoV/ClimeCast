b027129f74b6bb26e37517bda11f3736da162f00
pedro <pedro.arellano2001@gmail.com>
2024-02-10 17:01:26 +0000

    ask for location permission

---
+++ app/src/main/java/com/example/climecast/data/providers/LocationProvider.kt
@@ -0,0 +1,7 @@
+package com.example.climecast.data.providers
+
+import javax.inject.Inject
+
+class LocationProvider @Inject constructor() {
+    fun getLocation(){}
+}
--- app/src/main/java/com/example/climecast/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/climecast/ui/main/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.climecast.ui.main
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.climecast.R
 import com.example.climecast.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import android.Manifest
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +31,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
+        permissionsState()
         initNavigation()
     }
 
+    private fun permissionsState() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) -> {
+            }
+
+            else -> {
+                requestPermissionLauncher =
+                    registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                        if (isGranted) {
+                        } else {
+                        }
+                    }
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+        }
+    }
+
     private fun initNavigation() {
-        val navHost = supportFragmentManager.findFragmentById(R.id.mainFragmentContainer) as NavHostFragment
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.mainFragmentContainer) as NavHostFragment
         navController = navHost.navController
         binding.bottomNavView.setupWithNavController(navController)
     }
